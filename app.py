89afcec76117a5c0ab61e14789a2c97b3cea27f5
Bercut Remus-Gabriel <remus.bercut@stud.fils.upb.ro>
2026-04-27 20:26:37 +0300

    4 27 2026

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.set_page_config(page_title="Find My Type AI", layout="wide")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 if "all_images" not in st.session_state:
-    FEMALE_DIR = "img_align_celeba"
+    FEMALE_DIR = "females"
     images = sorted(glob.glob(os.path.join(FEMALE_DIR, "*.jpg")))
     random.shuffle(images)
     st.session_state.all_images = images
